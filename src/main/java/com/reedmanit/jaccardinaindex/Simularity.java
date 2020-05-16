199d3a7eeb2c2c4377e8ae8c4d6d58be2c73bf2c
PaulReedman <preedman@gmail.com>
2020-05-16 10:07:01 +1000

    v0.2 Add interface

--- src/main/java/com/reedmanit/jaccardinaindex/Simularity.java
+++ src/main/java/com/reedmanit/jaccardinaindex/Simularity.java
@@ -13,7 +13,7 @@ import java.util.Set;
  *
  * @author paul
  */
-public class Simularity {
+public class Simularity implements SimularityAlgorithm {
 
     private Double similarScore;
 
---
+++ src/main/java/com/reedmanit/jaccardinaindex/SimularityAlgorithm.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.reedmanit.jaccardinaindex;
+
+/**
+ *
+ * @author paul
+ */
+public interface SimularityAlgorithm {
+    
+    public void calculate(CharSequence A, CharSequence B);
+    
+}
